9fef5bc047fb67fcc462f5a6ff0f634670b24fac
emssssssssss <emmamatteuccii@gmail.com>
2025-05-24 12:11:56 +0200

    aggiornato

--- src/main/java/it/uniroma3/it/repository/DisponibilitaBigliettoRepository.java
+++ src/main/java/it/uniroma3/it/repository/DisponibilitaBigliettoRepository.java
@@ -3,7 +3,7 @@ package it.uniroma3.it.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.model.DisponibilitaBiglietto;
-
+//
 
 public interface DisponibilitaBigliettoRepository extends CrudRepository<DisponibilitaBiglietto, Long>{
 
