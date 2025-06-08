fa8187a82d33365b5a807096aceaae08bdbfc466
emssssssssss <emmamatteuccii@gmail.com>
2025-06-08 12:17:04 +0200

    sistemando classi museo e homepage

--- src/main/java/it/uniroma3/controller/MuseoController.java
+++ src/main/java/it/uniroma3/controller/MuseoController.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import it.uniroma3.service.MuseoService;
+import it.uniroma3.model.Museo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -14,11 +19,28 @@ public class MuseoController {
     @Autowired 
     private MuseoService museoService;
 
+
     @GetMapping("/")
-    public String getMuseo(Model model) {
-        Long id = (long) 1;
-        model.addAttribute("museo", this.museoService.getMuseo(id));
+    public String mostraHomepage(Model model) {
+        Long id = 1L;
+        model.addAttribute("museo", museoService.getMuseoUnico());
         return "homepage";
     }
+
+    //pagina per lo staff per modificare il museo
+    @PostMapping("/staff/museo/modifica")
+    public String mostraFormModificaMuseo(Model model) {
+        model.addAttribute("museo", museoService.getMuseoUnico());
+        return "staff/modificaMuseo";
+    }
+
+    //salva modifiche
+    @PostMapping("/staff/museo/modifica")
+    public String aggiornaMuseo(@ModelAttribute("museo") Museo museo, Model model) {
+        museoService.aggiornaMuseo(museo);
+        return "redirect:/staff/museo/modifica?successo";
+    }
+    
+    
     
 }
